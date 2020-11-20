8fa054940d53cfd6e890dd34138d88461ca8df44
Saurabh <Saurabh@DESKTOP-FDSGUKR>
2020-11-20 06:19:47 +0530

    Added changes for Compliance scorecard graph

--- src/main/java/com/eai/idss/dao/IndustryMasterDaoImpl.java
+++ src/main/java/com/eai/idss/dao/IndustryMasterDaoImpl.java
@@ -1,7 +1,16 @@
 package com.eai.idss.dao;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.jboss.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -13,6 +22,9 @@ import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
 
 import com.eai.idss.model.IndustryMaster;
+import com.eai.idss.model.LegalDataMaster;
+import com.eai.idss.util.IDSSUtil;
+import com.eai.idss.vo.ComlianceScoreFilter;
 import com.eai.idss.vo.IndustryMasterRequest;
 
 @Repository
@@ -20,7 +32,8 @@ public class IndustryMasterDaoImpl implements IndustryMasterDao {
 	
 	@Autowired
 	MongoTemplate mongoTemplate;
-	
+	public static final Logger logger = Logger.getLogger(IndustryMasterDaoImpl.class);
+
 	public List<IndustryMaster> getIndustryMasterPaginatedRecords(IndustryMasterRequest imr ,Pageable page){
 		
 		
@@ -59,4 +72,93 @@ public class IndustryMasterDaoImpl implements IndustryMasterDao {
 		
 		return true;
 	}
+	
+	public Map<String, List<LegalDataMaster>> getByIndustryNameComplianceScoreData(ComlianceScoreFilter cf,
+			Pageable page) {
+		try {
+			
+
+			logger.info("getComplainceScoreCardData");
+
+			Map<String, String> daysMap = IDSSUtil.getPastDaysMapForLegal();
+			Map<String, List<LegalDataMaster>> tileMap = new LinkedHashMap<String, List<LegalDataMaster>>();
+
+			if (null != cf) {
+				
+				for (String days : daysMap.keySet()) {
+					Query query = new Query().with(page);
+					logger.info("getComplainceScoreCardData : " + days);
+
+					query.addCriteria(Criteria.where("visitedDate")
+							.gte(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSSZ").parse(days + " 00:00:00.000+0000")));
+
+					if (StringUtils.hasText(cf.getIndustryName()))
+						query.addCriteria(Criteria.where("industryName").is(cf.getIndustryName()));
+
+					System.out.println(mongoTemplate.count(query, LegalDataMaster.class));
+
+					List<LegalDataMaster> filteredLegalList = mongoTemplate.find(query, LegalDataMaster.class);
+					Page<LegalDataMaster> cPage = PageableExecutionUtils.getPage(filteredLegalList, page,
+							() -> mongoTemplate.count(query, LegalDataMaster.class));
+
+					List<LegalDataMaster> ok = new ArrayList<LegalDataMaster>(cPage.toList());
+
+					tileMap.put(daysMap.get(days), ok);
+
+				}
+				return tileMap;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	public List<LegalDataMaster> getDataBetweenDuration(ComlianceScoreFilter cf, Pageable page) {
+		try {
+			Query query = new Query().with(page);
+			if (null != cf) {
+				String[] d = cf.getDuration().split(",");
+
+				LocalDateTime currentTime = LocalDateTime.now();
+
+				LocalDate startDate = LocalDate.parse(d[0]);
+				long noOfDaysBetweenStartDate = ChronoUnit.DAYS.between(startDate, currentTime);
+
+				LocalDate endDate = LocalDate.parse(d[1]);
+				long noOfDaysBetweenEndDate = ChronoUnit.DAYS.between(endDate, currentTime);
+
+				logger.info("getComplainceScoreCardCustomFilterData");
+
+				LocalDateTime fromDate = currentTime.minusDays(noOfDaysBetweenStartDate);
+				String fromDay = fromDate.format(DateTimeFormatter.ISO_LOCAL_DATE);
+
+				LocalDateTime toDate = currentTime.minusDays(noOfDaysBetweenEndDate);
+				String toDay = toDate.format(DateTimeFormatter.ISO_LOCAL_DATE);
+
+				query.addCriteria(Criteria.where("visitedDate")
+						.gte(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSSZ").parse(fromDay + " 00:00:00.000+0000"))
+						.lt(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSSZ").parse(toDay + " 00:00:00.000+0000")));
+
+				if (StringUtils.hasText(cf.getIndustryName()))
+					query.addCriteria(Criteria.where("IndustryName").is(cf.getIndustryName()));
+
+				System.out.println(mongoTemplate.count(query, LegalDataMaster.class));
+
+				List<LegalDataMaster> filteredComplianceScorecardList = mongoTemplate.find(query,
+						LegalDataMaster.class);
+
+				Page<LegalDataMaster> cPage = PageableExecutionUtils.getPage(filteredComplianceScorecardList, page,
+						() -> mongoTemplate.count(query, LegalDataMaster.class));
+
+				return cPage.toList();
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+
 }
