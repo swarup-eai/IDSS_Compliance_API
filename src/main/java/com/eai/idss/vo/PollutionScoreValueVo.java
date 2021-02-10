38a6c698ae6c7d019906814f07c037d092b652b2
unknown <swarup.shridhar@blucognition.com>
2021-02-10 20:46:01 +0530

    pollution graph all years data with dummy obj

--- src/main/java/com/eai/idss/vo/PollutionScoreValueVo.java
+++ src/main/java/com/eai/idss/vo/PollutionScoreValueVo.java
@@ -2,13 +2,23 @@ package com.eai.idss.vo;
 
 public class PollutionScoreValueVo {
 
-	private String monthYear;
+	private String year;
 	private double value;
-	public String getMonthYear() {
-		return monthYear;
+	
+	public  PollutionScoreValueVo() {
+		
 	}
-	public void setMonthYear(String monthYear) {
-		this.monthYear = monthYear;
+	
+	public  PollutionScoreValueVo(String y,double v) {
+		this.year=y;
+		this.value=v;
+	}
+	
+	public String getYear() {
+		return year;
+	}
+	public void setYear(String year) {
+		this.year = year;
 	}
 	public double getValue() {
 		return value;
